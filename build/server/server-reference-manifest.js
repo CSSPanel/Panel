f39a42e3b9aa1c58d5b6be2755be0f175a0223fb
Amir Eliz <tripl3n2@gmail.com>
2024-07-12 21:06:26 +0300

    log reportedUser

--- build/server/server-reference-manifest.js
+++ build/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"3EQR7hLpsjdRFSyo0kqKS7D/YxQwlOwZDLXxfmCOoYU=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"haYUkhTMz3Esj6Etasdr1/VeZfBIvKWMlf20EdsFooQ=\"}"
